8d866b7ecad275d6f44d5ec2558bb728356b8ba0
Matheus Acfer <matheusacfer@EU3146037018865.local>
2025-02-20 13:41:08 -0300

    disable obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,57 +1,57 @@
 return {
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*",
-    lazy = false,
-    ft = "markdown",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("obsidian").setup({
-        workspaces = {
-          {
-            name = "personal",
-            path = "~/Documents/Obsidian-Vault/",
-          },
-        },
-        -- templates = {
-        --   subdir = "templates",
-        --   date_format = "%d-%m-%Y",
-        --   time_format = "%H:%M:%S",
-        -- },
-        daily_notes = {
-          folder = "notes/dailies",
-          date_format = "%d-%m-%Y",
-          alias_format = "%B %-d, %Y",
-          default_tags = { "daily-notes" },
-          template = nil,
-        },
-
-        note_id_func = function(title)
-          -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-          -- In this case a note with the title 'My new note' will be given an ID that looks
-          -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-          local suffix = ""
-          if title ~= nil then
-            -- If title is given, transform it into valid file name.
-            suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-          else
-            -- If title is nil, just add 4 random uppercase letters to the suffix.
-            for _ = 1, 4 do
-              suffix = suffix .. string.char(math.random(65, 90))
-            end
-          end
-          return tostring(os.time()) .. "-" .. suffix
-        end,
-
-        completion = {
-          nvim_cmp = true,
-          min_chars = 2,
-        },
-      })
-    end,
-  },
+  -- {
+  --   "epwalsh/obsidian.nvim",
+  --   version = "*",
+  --   lazy = false,
+  --   ft = "markdown",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --   config = function()
+  --     require("obsidian").setup({
+  --       workspaces = {
+  --         {
+  --           name = "personal",
+  --           path = "~/Documents/Obsidian-Vault/",
+  --         },
+  --       },
+  --       -- templates = {
+  --       --   subdir = "templates",
+  --       --   date_format = "%d-%m-%Y",
+  --       --   time_format = "%H:%M:%S",
+  --       -- },
+  --       daily_notes = {
+  --         folder = "notes/dailies",
+  --         date_format = "%d-%m-%Y",
+  --         alias_format = "%B %-d, %Y",
+  --         default_tags = { "daily-notes" },
+  --         template = nil,
+  --       },
+  --
+  --       note_id_func = function(title)
+  --         -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+  --         -- In this case a note with the title 'My new note' will be given an ID that looks
+  --         -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+  --         local suffix = ""
+  --         if title ~= nil then
+  --           -- If title is given, transform it into valid file name.
+  --           suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+  --         else
+  --           -- If title is nil, just add 4 random uppercase letters to the suffix.
+  --           for _ = 1, 4 do
+  --             suffix = suffix .. string.char(math.random(65, 90))
+  --           end
+  --         end
+  --         return tostring(os.time()) .. "-" .. suffix
+  --       end,
+  --
+  --       completion = {
+  --         nvim_cmp = true,
+  --         min_chars = 2,
+  --       },
+  --     })
+  --   end,
+  -- },
   -- {
   --   "iamcco/markdown-preview.nvim",
   --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
